1ed27895303508b3c139c250b3dbcc91dadb9de3
chuchunchi <chuchun1231@gmail.com>
2023-10-10 12:53:05 +0800

    :pencil: add hw0

---
+++ hw0/pi.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+/*
+number_in_circle = 0;
+for ( toss = 0; toss < number_of_tosses; toss ++) {
+    x = random double between -1 and 1;
+    y = random double between -1 and 1;
+    distance_squared = x * x + y * y;
+    if ( distance_squared <= 1)
+        number_in_circle++;
+}
+pi_estimate = 4 * number_in_circle /(( double ) number_of_tosses);
+*/
+int main(){
+    long long int number_in_circle = 0;
+    long long int number_of_tosses = 10e8;
+    for (long long int toss = 0; toss < number_of_tosses; toss ++) {
+        long double x = (double) rand()*2 / RAND_MAX - 1;
+        long double y = (double) rand()*2 / RAND_MAX - 1;
+        double distance_squared = x * x + y * y;
+        if ( distance_squared <= 1){
+            number_in_circle++;
+        }
+    }
+    cout << double( 4 * number_in_circle / (double) number_of_tosses);
+}
