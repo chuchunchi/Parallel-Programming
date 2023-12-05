7cc0d67657d2c910a7658b52372ca59ef4ebf27b
chuchunchi <chuchun1231@gmail.com>
2023-12-06 00:38:35 +0800

    add hello mpi and pi_block_linear

--- HW4/src/pi_block_linear.cc
+++ HW4/src/pi_block_linear.cc
@@ -5,6 +5,23 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+
+int monte_carlo(long long int my_n_tosses, int &world_rank) {
+    long long int my_number_in_circle = 0;
+    unsigned int seed = world_rank;
+    for (long long int toss = 0; toss < my_n_tosses; toss ++) {
+        double x = ((double) rand_r(&seed)*2 / RAND_MAX) - 1.0;
+        double y = ((double) rand_r(&seed)*2 / RAND_MAX) - 1.0;
+        if ( x * x + y * y <= 1){
+            my_number_in_circle++;
+        }
+    }
+
+    return my_number_in_circle;
+
+    
+}
+
 int main(int argc, char **argv)
 {
     // --- DON'T TOUCH ---
@@ -14,22 +31,35 @@ int main(int argc, char **argv)
     long long int tosses = atoi(argv[1]);
     int world_rank, world_size;
     // ---
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
+    long long int number_in_circle = monte_carlo(tosses / world_size, world_rank);
+    long long int number_in_circle_sum = 0;
+    
     // TODO: init MPI
 
     if (world_rank > 0)
     {
         // TODO: handle workers
+        MPI_Send(&number_in_circle, 1, MPI_LONG_LONG_INT, 0, 0, MPI_COMM_WORLD);
     }
     else if (world_rank == 0)
     {
         // TODO: master
+        number_in_circle_sum += number_in_circle;
+        for (int i = 1; i < world_size; i++)
+        {
+            long long int number_in_circle_others;
+            MPI_Recv(&number_in_circle_others, 1, MPI_LONG_LONG_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            number_in_circle_sum += number_in_circle_others;
+        }
     }
 
     if (world_rank == 0)
     {
         // TODO: process PI result
-
+        pi_result = double( 4 * number_in_circle_sum / (double) tosses );
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
         printf("%lf\n", pi_result);
