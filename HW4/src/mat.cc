d5c040c9ffcff3d49e1ce5f2e2eb8259e2d32b74
chuchunchi <chuchun1231@gmail.com>
2023-12-07 23:30:14 +0800

    done hw4

--- HW4/src/mat.cc
+++ HW4/src/mat.cc
@@ -1,50 +1,5 @@
+#include <stdio.h>
 #include <mpi.h>
-#include <cstdio>
-
-// *********************************************
-// ** ATTENTION: YOU CANNOT MODIFY THIS FILE. **
-// *********************************************
-
-// Read size of matrix_a and matrix_b (n, m, l) and whole data of matrixes from stdin
-//
-// n_ptr:     pointer to n
-// m_ptr:     pointer to m
-// l_ptr:     pointer to l
-// a_mat_ptr: pointer to matrix a (a should be a continuous memory space for placing n * m elements of int)
-// b_mat_ptr: pointer to matrix b (b should be a continuous memory space for placing m * l elements of int)
-void construct_matrices(int *n_ptr, int *m_ptr, int *l_ptr,
-                        int **a_mat_ptr, int **b_mat_ptr);
-
-// Just matrix multiplication (your should output the result in this function)
-// 
-// n:     row number of matrix a
-// m:     col number of matrix a / row number of matrix b
-// l:     col number of matrix b
-// a_mat: a continuous memory placing n * m elements of int
-// b_mat: a continuous memory placing m * l elements of int
-void matrix_multiply(const int n, const int m, const int l,
-                     const int *a_mat, const int *b_mat);
-
-// Remember to release your allocated memory
-void destruct_matrices(int *a_mat, int *b_mat);
-
-int main () {
-    int n, m, l;
-    int *a_mat, *b_mat;
-
-    MPI_Init(NULL, NULL);
-    double start_time = MPI_Wtime();
-
-    construct_matrices(&n, &m, &l, &a_mat, &b_mat);
-    matrix_multiply(n, m, l, a_mat, b_mat);
-    destruct_matrices(a_mat, b_mat);
-
-    double end_time = MPI_Wtime();
-    MPI_Finalize();
-    printf("MPI running time: %lf Seconds\n", end_time - start_time);
-
-    return 0;
-}
 
 void construct_matrices(int *n_ptr, int *m_ptr, int *l_ptr,
                    int **a_mat_ptr, int **b_mat_ptr) {
@@ -57,6 +12,7 @@ void construct_matrices(int *n_ptr, int *m_ptr, int *l_ptr,
 
     if(world_rank == 0) {
         scanf("%d %d %d", n_ptr, m_ptr, l_ptr);
+        printf("n = %d, m = %d, l = %d\n", *n_ptr, *m_ptr, *l_ptr);
     }
 
     MPI_Bcast(n_ptr, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -103,11 +59,11 @@ void matrix_multiply(const int n, const int m, const int l,
 
     for(i = start ; i < end ; i++, a_idx += m){
         c_idx = i * l;
+        b_idx = 0;
         for(j = 0 ; j < l ; j++){
-            b_idx = j;
             for(k = 0 ; k < m ; k++){
                 C[c_idx] += a_mat[a_idx + k] * b_mat[b_idx];
-                b_idx += l;
+                b_idx++;
             }
             c_idx++;
         }
--- HW4/src/pi_one_side.cc
+++ HW4/src/pi_one_side.cc
@@ -28,7 +28,8 @@ int main(int argc, char **argv)
     long long int tosses = atoi(argv[1]);
     int world_rank, world_size;
     // ---
-
+    // MPI_Comm nodecomm;                                                          
+    // MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &nodecomm);    
     MPI_Win win;
 
     // TODO: MPI init
@@ -38,34 +39,58 @@ int main(int argc, char **argv)
     long long int number_in_circle = monte_carlo(tosses / world_size, world_rank);
     
     // Allocate shared memory
-    long long int *number_in_circle_sum;
-    MPI_Win_allocate_shared(sizeof(long long int), sizeof(long long int), MPI_INFO_NULL, MPI_COMM_WORLD, &number_in_circle_sum, &win);
+    // long long int *number_in_circle_sum;
+    // MPI_Win_allocate_shared(sizeof(long long int), sizeof(long long int), MPI_INFO_NULL, nodecomm, &number_in_circle_sum, &win);
 
-    // Initialize the shared memory
+    // // Initialize the shared memory
+    // if (world_rank == 0) {
+    //     *number_in_circle_sum = 0;
+    // }
+
+    // // Start the access/exposure epoch
+    // MPI_Win_fence(0, win);
+
+    // // All processes, including the root, accumulate their results into the shared memory
+    // MPI_Accumulate(&number_in_circle, 1, MPI_LONG_LONG_INT, 0, 0, 1, MPI_LONG_LONG_INT, MPI_SUM, win);
+
+    // // End the access/exposure epoch
+    // MPI_Win_fence(0, win);
+    long long int *number_in_circle_sum;
     if (world_rank == 0) {
-        *number_in_circle_sum = 0;
+        MPI_Alloc_mem(world_size * sizeof(long long int), MPI_INFO_NULL, &number_in_circle_sum);
+        for (int i = 0; i < world_size; i++) {
+            number_in_circle_sum[i] = 0;
+        }
+        MPI_Win_create(number_in_circle_sum, world_size * sizeof(long long int), sizeof(long long int), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+    } else {
+        MPI_Win_create(NULL, 0, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &win);
     }
 
-    // Start the access/exposure epoch
+    // Add a while loop to prevent deadlock
+    int flag = 0;
+    MPI_Win_lock(MPI_LOCK_SHARED, 0, 0, win);
+    MPI_Put(&number_in_circle, 1, MPI_LONG_LONG_INT, 0, world_rank, 1, MPI_LONG_LONG_INT, win);
+    MPI_Win_flush(0, win);  // Ensure that the MPI_Put operation is complete
+    MPI_Win_unlock(0, win);
     MPI_Win_fence(0, win);
-
-    // All processes, including the root, accumulate their results into the shared memory
-    MPI_Accumulate(&number_in_circle, 1, MPI_LONG_LONG_INT, 0, 0, 1, MPI_LONG_LONG_INT, MPI_SUM, win);
-
-    // End the access/exposure epoch
-    MPI_Win_fence(0, win);
-
+ 
     if (world_rank == 0)
     {
         // TODO: handle PI result
-        pi_result = double( 4 * (*number_in_circle_sum) / (double) tosses );
+        long long int total_number_in_circle = 0;
+        for (int i = 0; i < world_size; i++) {
+            total_number_in_circle += number_in_circle_sum[i];
+        }
+        pi_result = 4 * total_number_in_circle / (double) tosses;
+        // pi_result = double( 4 * (*number_in_circle_sum) / (double) tosses );
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
         printf("%lf\n", pi_result);
         printf("MPI running time: %lf Seconds\n", end_time - start_time);
         // ---
+        MPI_Free_mem(number_in_circle_sum);
     }
-
+    
     MPI_Win_free(&win);
     MPI_Finalize();
     return 0;
